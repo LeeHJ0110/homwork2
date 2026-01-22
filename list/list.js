2accb68edc84d1b1afc3cb6433792e5929598606
DESKTOP-HTUSJ3D\user1 <adskln;owiewoi@aiuhkds.com>
2026-01-22 09:13:21 +0900

    last

--- list/list.js
+++ list/list.js
@@ -3,26 +3,32 @@ function displayPoList(){
     if(!boardVoList){
         return;
     }
+    console.log(boardVoList);
+    
+
     let str = "";
     for(let i = 0 ; i < boardVoList.length ; i++){
         const vo = boardVoList[i];
         str += `
             <tr>
-                <th> ${i} </th>
-                <td> ${vo.title} </td>
-                <td><button onclick="">수정 밑 상세보기</button></td>
+                <th>${i}</th>
+                <td>${vo.name}</td>
+                <td>${vo.type.join(", ")}</td>
+                <td>
+                    <button onclick="setPoNo(${i})">수정 및 상세보기</button>
+                </td>
             </tr>
         `;
     }
 
     const tbodyTag = document.querySelector("main table tbody");
     tbodyTag.innerHTML = str;
-
 }
+
 
 function setPoNo(no){
     localStorage.setItem("selectedPoNo", no);
-    location.href="./edit.html";
+    location.href="../make/make.html";
 }
 
 window.onload = function(){
--- make/make.js
+++ make/make.js
@@ -1,5 +1,82 @@
-function countCheck(){
-    const chkbox = document.querySelector("input[name=check]");
-    const checkcount = 0;
+let checkCnt = 0;
+
+function countCheck(checkTag){
+    if(checkTag.checked){
+        checkCnt++;
+    }else{
+        checkCnt--;
+    }
+    
+    console.log(checkCnt);
+    
+    if (checkCnt > 2) {
+        checkTag.checked = false;
+        checkCnt--;
+        alert("타입은 최대 2개");
+    }
 
 }
+
+function insertBoard() {
+    const name = document.querySelector("input[name=name]").value.trim();
+    const chara = document.querySelector("input[name=chara]").value.trim();
+
+    const checkedTypes = [];
+    const checkedboxs = document.querySelectorAll("input[name=check]:checked");
+    for(checked of checkedboxs){
+        checkedTypes.push(checked.value);
+    }
+
+    if (!name || !chara || checkedTypes.length === 0) {
+        alert("모든 값을 입력해주세요.");
+        return;
+    }
+
+    const vo = {
+        name: name,
+        chara: chara,
+        type: checkedTypes
+    };
+
+    let boardVoList = JSON.parse(localStorage.getItem("boardVoList"));
+    if (!boardVoList) {
+        boardVoList = [];
+    }
+
+    const selectedPoNo = localStorage.getItem("selectedPoNo");
+
+    if (selectedPoNo === null || selectedPoNo < 0) {
+        // 새로 만들기
+        boardVoList.push(vo);
+    } else {
+        // 수정
+        boardVoList[selectedPoNo] = vo;
+    }
+    localStorage.setItem("boardVoList", JSON.stringify(boardVoList));
+    
+
+    location.href = "../list/list.html";
+}
+
+function loadEditData(no) {
+    const boardVoList = JSON.parse(localStorage.getItem("boardVoList"));
+    const vo = boardVoList[no];
+
+    document.querySelector("input[name=name]").value = vo.name;
+    document.querySelector("input[name=chara]").value = vo.chara;
+
+    const checkboxes = document.querySelectorAll("input[name=check]");
+    for(box of checkboxes){
+        box.checked = vo.type.includes(box.value);
+        if(box.checked) checkCnt++;
+    }
+
+}
+
+window.onload = function () {
+    const selectedPoNo = localStorage.getItem("selectedPoNo");
+
+    if (selectedPoNo !== null) {
+        loadEditData(selectedPoNo);
+    }
+};
